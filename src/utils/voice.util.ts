4875db9afeb42338986b86cdea76cc004898ddd8
Felix <felix@pojtinger.com>
2018-12-02 18:37:58 +0100

    feat(learn-chinese-platform-playground): Add util to get vector for a hanzi

--- src/utils/voice.util.ts
+++ src/utils/voice.util.ts
@@ -4,12 +4,14 @@ import * as googleTTS from "google-tts-api";
  * Get voice for hanzi
  * @param hanzi Hanzi to get voice for
  */
-const getVoiceForHanzi = async hanzi => googleTTS(hanzi, "zh", 1);
+const getVoiceForHanzi = async (hanzi: string): Promise<string> =>
+  googleTTS(hanzi, "zh", 1);
 
 /**
  * Get voice for pinyin
  * @param pinyin Pinyin to get voice for
  */
-const getVoiceForPinyin = async pinyin => getVoiceForHanzi(pinyin);
+const getVoiceForPinyin = async (pinyin: string): Promise<string> =>
+  getVoiceForHanzi(pinyin);
 
 export { getVoiceForHanzi, getVoiceForPinyin };
