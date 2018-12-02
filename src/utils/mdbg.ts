c5c10402a4ca59aa058d8fb83b81311fd0e3bb83
Felix <felix@pojtinger.com>
2018-12-02 02:19:19 +0100

    feat(learn-chinese-platform-playground): add utilities for getting english for hanzi

--- src/utils/mdbg.ts
+++ src/utils/mdbg.ts
@@ -15,8 +15,30 @@ const getPinyinForHanzi = async hanzi =>
       )
     );
 
+const getEnglishForHanzi = async hanzi =>
+  mdbg
+    .getByHanzi(hanzi)
+    .then(pinyin =>
+      Object.keys(pinyin.definitions).map(
+        pinyinOption => pinyin.definitions[pinyinOption].translations
+      )
+    );
+
+const getEnglishForPinyin = async pinyin =>
+  mdbg
+    .getByPinyin(pinyin)
+    .then(hanzis =>
+      hanzis.map(hanzi =>
+        Object.keys(hanzi.definitions).map(
+          pinyinOption => hanzi.definitions[pinyinOption].translations
+        )
+      )
+    );
+
 export {
   getSimplifiedHanziForPinyin,
   getTraditionalHanziForPinyin,
-  getPinyinForHanzi
+  getPinyinForHanzi,
+  getEnglishForHanzi,
+  getEnglishForPinyin
 };
