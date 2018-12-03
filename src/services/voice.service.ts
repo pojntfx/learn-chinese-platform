ffc6c046063c3341c454385f24120f51490ad09c
Felix <felix@pojtinger.com>
2018-12-03 21:55:12 +0100

    feat(learn-chinese-platform-playground): add female and male voices using alternative tts provider

--- src/services/voice.service.ts
+++ src/services/voice.service.ts
@@ -1,21 +1,53 @@
-import { getVoiceForHanzi, getVoiceForPinyin } from "../utils/voice.util";
+import {
+  getVoiceForPinyinAlt,
+  getVoiceForHanziAlt,
+  getFemaleVoiceForHanzi,
+  getFemaleVoicesForPinyin,
+  getMaleVoicesForPinyin
+} from "../utils/voice.util";
 import { ServiceSchema } from "moleculer";
 
 const Voice: ServiceSchema = {
   name: "voice",
   version: 1,
   actions: {
-    getVoiceForHanzi: {
+    getFemaleVoiceForHanzi: {
       params: {
         hanzi: "string"
       },
-      handler: ctx => getVoiceForHanzi(ctx.params.hanzi)
+      handler: ctx => getFemaleVoiceForHanzi(ctx.params.hanzi)
     },
-    getVoiceForPinyin: {
+    getMaleVoiceForHanzi: {
+      params: {
+        hanzi: "string"
+      },
+      handler: ctx => getFemaleVoiceForHanzi(ctx.params.hanzi)
+    },
+    getFemaleVoicesForPinyin: {
       params: {
         pinyin: "string"
       },
-      handler: ctx => getVoiceForPinyin(ctx.params.pinyin)
+      handler: async ctx =>
+        await Promise.all(await getFemaleVoicesForPinyin(ctx.params.pinyin))
+    },
+    getMaleVoicesForPinyin: {
+      params: {
+        pinyin: "string"
+      },
+      handler: async ctx =>
+        await Promise.all(await getMaleVoicesForPinyin(ctx.params.pinyin))
+    },
+    getVoiceForHanziAlt: {
+      params: {
+        hanzi: "string"
+      },
+      handler: ctx => getVoiceForHanziAlt(ctx.params.hanzi)
+    },
+    getVoiceForPinyinAlt: {
+      params: {
+        pinyin: "string"
+      },
+      handler: ctx => getVoiceForPinyinAlt(ctx.params.pinyin)
     }
   }
 };
--- src/utils/voice.util.ts
+++ src/utils/voice.util.ts
@@ -1,17 +1,64 @@
 import * as googleTTS from "google-tts-api";
+import { buildUrl, voices, languages } from "oddcast-tts-demo";
+import { getSimplifiedHanziForPinyin } from "./translator.util";
 
 /**
- * Get voice for hanzi
+ * Get female voice for a hanzi
+ * @param hanzi Hanzi to get female voice for
+ */
+const getFemaleVoiceForHanzi = async (hanzi: string): Promise<string> =>
+  buildUrl(hanzi, {
+    ...voices.tingting,
+    language: languages.zh
+  });
+
+/**
+ * Get male voice for hanzi
+ * @param hanzi Hanzi to get male voice for
+ */
+const getMaleVoiceForHanzi = async (hanzi: string): Promise<string> =>
+  buildUrl(hanzi, {
+    ...voices.liang,
+    language: languages.zh
+  });
+
+/**
+ * Get female voices for pinyin
+ * @param pinyin Pinyin to get female voices for
+ */
+const getFemaleVoicesForPinyin = async (pinyin: string) =>
+  getSimplifiedHanziForPinyin(pinyin).then(hanzi =>
+    hanzi.map(hanzi => getFemaleVoiceForHanzi(hanzi))
+  );
+
+/**
+ * Get male voices for pinyin
+ * @param pinyin Pinyin to get male voices for
+ */
+const getMaleVoicesForPinyin = async (pinyin: string) =>
+  getSimplifiedHanziForPinyin(pinyin).then(hanzi =>
+    hanzi.map(hanzi => getMaleVoiceForHanzi(hanzi))
+  );
+
+/**
+ * Get voice for a hanzi (alternative using `google-tts-api`)
  * @param hanzi Hanzi to get voice for
  */
-const getVoiceForHanzi = async (hanzi: string): Promise<string> =>
+const getVoiceForHanziAlt = async (hanzi: string): Promise<string> =>
   googleTTS(hanzi, "zh", 1);
 
 /**
- * Get voice for pinyin
- * @param pinyin Pinyin to get voice for
+ * Get voice for a hanzi (alternative using `google-tts-api`)
+ * @param pinyin Pinyin ot get voice for
  */
-const getVoiceForPinyin = async (pinyin: string): Promise<string> =>
-  getVoiceForHanzi(pinyin);
+const getVoiceForPinyinAlt = async (pinyin: string): Promise<string> =>
+  getVoiceForHanziAlt(pinyin);
 
-export { getVoiceForHanzi, getVoiceForPinyin };
+export {
+  getFemaleVoiceForHanzi,
+  getMaleVoiceForHanzi,
+  getFemaleVoicesForPinyin,
+  getMaleVoicesForPinyin,
+  getVoiceForHanziAlt,
+  getVoiceForPinyinAlt
+};
