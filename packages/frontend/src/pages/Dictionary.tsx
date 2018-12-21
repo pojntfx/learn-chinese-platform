1dea89b6736536ee89040e183708cb400591a73a
Felix Pojtinger <felix@pojtinger.com>
2018-12-21 18:05:28 +0100

    refactor(frontend): Add search bar

--- packages/frontend/src/pages/Dictionary.tsx
+++ packages/frontend/src/pages/Dictionary.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Component, RefObject } from "react";
 import { List } from "../downstream/Context/List";
 import { dictionary } from "../data/dictionary";
 import { navbar } from "../data/navbar";
@@ -6,27 +7,76 @@ import { footer } from "../data/footer";
 import { noscript } from "../data/noscript";
 import { shortcuts } from "../data/shortcuts";
 import { Link } from "../downstream/Link";
-import { IShellProps, Shell } from "@libresat/frontend-components";
+import { IShellProps, Shell, Header } from "@libresat/frontend-components";
 import { SettingsContainer } from "../containers/Settings";
 import { Subscribe } from "unstated";
+import { SearchContainer } from "../containers/Search";
+import { Wrapper as Search } from "../downstream/Context/Input/Wrapper";
+import { Sticky } from "semantic-ui-react";
 
-const Dictionary = (props: any) => (
-  <Shell
-    head={dictionary.head}
-    navbar={navbar as IShellProps["navbar"]}
-    footer={footer as IShellProps["footer"]}
-    noScript={noscript}
-    shortcuts={shortcuts}
-    background={dictionary.background}
-    linkComponent={Link as any}
-    {...props}
-  >
-    <Subscribe to={[SettingsContainer]}>
-      {({ state: { gatewayUrl } }: SettingsContainer) => (
-        <List endpoint={gatewayUrl} query="nǐ" />
-      )}
-    </Subscribe>
-  </Shell>
-);
+class Dictionary extends Component {
+  state = {
+    contextRef: {}
+  };
+
+  handleContextRef = (contextRef: any) => this.setState({ contextRef });
+
+  render() {
+    return (
+      <Shell
+        head={dictionary.head}
+        navbar={navbar as IShellProps["navbar"]}
+        footer={footer as IShellProps["footer"]}
+        noScript={noscript}
+        shortcuts={shortcuts}
+        background={dictionary.background}
+        linkComponent={Link as any}
+        {...this.props}
+      >
+        <Subscribe to={[SearchContainer]}>
+          {(search: SearchContainer) => (
+            <>
+              {!search.state.query && (
+                <Header
+                  as="h1"
+                  content="The Learn Chinese Platform Dictionary"
+                  subheader="Search for Hanzi, Pinyin or English."
+                />
+              )}
+              <Subscribe to={[SettingsContainer]}>
+                {(settings: SettingsContainer) => (
+                  <div ref={this.handleContextRef}>
+                    <Sticky context={this.state.contextRef} offset={50}>
+                      <Search
+                        input={{
+                          onChange: (e: any) => search.setQuery(e.target.value),
+                          value: search.state.query,
+                          autoFocus: true,
+                          placeholder: "Search for Pinyin, Hanzi or English",
+                          fluid: true,
+                          icon: "search"
+                        }}
+                        options={{
+                          onClick: settings.togglePreciseSearch,
+                          precise: settings.state.searchIsPrecise
+                        }}
+                      />
+                    </Sticky>
+                    {search.state.query && (
+                      <List
+                        endpoint={settings.state.gatewayUrl}
+                        query={search.state.query}
+                      />
+                    )}
+                  </div>
+                )}
+              </Subscribe>
+            </>
+          )}
+        </Subscribe>
+      </Shell>
+    );
+  }
+}
 
 export { Dictionary };
